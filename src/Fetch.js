b18c885b87d755cee0c633674747d1a80438c629
Lucas <lucas@sopheos.com>
2025-09-08 08:23:54 +0200

    [+] Helpers Fetch

---
+++ src/Fetch.js
@@ -0,0 +1,214 @@
+/**
+ * @typedef HttpResponse
+ * @type {object}
+ * @property {number} status
+ * @property {any} body
+ *
+ * @typedef Options
+ * @type {object}
+ * @property {string} [baseUrl]
+ * @property {string} [method]
+ * @property {AbortSignal} [signal]
+ * @property {RequestCredentials} [credentials]
+ * @property {?Object.<string, any>} [headers]
+ * @property {?Object.<string, any>} [query]
+ * @property {?Object.<string, any>} [body]
+ * @property {?Object.<string, any>} [form]
+ * @property {?Object.<string, any>} [json]
+ */
+
+export default class Fetch {
+  options = {};
+
+  /**
+   * @param {Options} options
+   */
+  constructor(options = {}) {
+    this.options = options;
+  }
+
+  /**
+   * @param {string} url
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  async send(url, options = {}) {
+    const config = { ...this.options, ...options };
+
+    // Build url ----------------------------------------------------------------------------------
+    const input = new URL(url, config.baseUrl ?? location.origin);
+
+    // add query ----------------------------------------------------------------------------------
+    if (config.query) {
+      for (const key in config.query) {
+        let data = config.query[key];
+        if (Array.isArray(data)) {
+          for (const [index, value] of data.entries()) {
+            input.searchParams.append(`${key}[${index}]`, value);
+          }
+        } else if (data !== null) {
+          input.searchParams.append(key, data);
+        }
+      }
+    }
+
+    // Build request init -------------------------------------------------------------------------
+
+    const init = {
+      method: config.method ?? "GET",
+      credentials: config.credentials ?? "omit",
+      signal: config.signal ?? null,
+      headers: new Headers(config.headers ?? {}),
+    };
+
+    // add body  ----------------------------------------------------------------------------------
+    if (config.json) {
+      init.headers.set("Content-Type", "application/json");
+      init.body = JSON.stringify(config.json);
+    } else if (config.form) {
+      init.body = new FormData();
+      for (const key in config.form) {
+        init.body.append(key, config.form[key]);
+      }
+    }
+
+    return fetch(input, init)
+      .then((res) => Fetch.handleResponse(res))
+      .catch((e) => {
+        if (e instanceof HttpException) {
+          throw e;
+        }
+
+        throw new HttpException({
+          status: 500,
+          data: {
+            status: 500,
+            error: navigator?.onLine ? "fetch" : "offline",
+          },
+        });
+      });
+  }
+
+  /**
+   * Handle Fetch API response
+   * @param {Response} response
+   * @returns {Promise<HttpResponse>}
+   * @throws {HttpException}
+   */
+  static async handleResponse(response) {
+    const body = await Fetch.parseBody(response);
+    if (response.ok) {
+      return { status: response.status, body };
+    }
+    throw new HttpException({
+      status: response.status,
+      data: body,
+    });
+  }
+
+  /**
+   * Parse the content of a Fetch API response thanks to the Content-Type header
+   * @param {Response} response
+   * @returns {Promise<any>}
+   */
+  static async parseBody(response) {
+    if (!response.body) {
+      return null;
+    }
+
+    const type = response.headers.get("Content-Type");
+    if (type) {
+      if (type.match(/.*application\/json.*/g)) {
+        return response.json();
+      }
+      if (type.match(/.*application\/pdf.*/g)) {
+        return response.blob();
+      }
+      if (type.match(/.*image\/.*/g)) {
+        return response.blob();
+      }
+    }
+    return response.text();
+  }
+
+  // HELPERS ----------------------------------------------------------------------------------------
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  get(url = "/", data = null, options = {}) {
+    options.method = "GET";
+    options.query = data;
+    return this.send(url, options);
+  }
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  delete(url = "/", data = null, options = {}) {
+    options.method = "DELETE";
+    options.query = data;
+    return this.send(url, options);
+  }
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  post(url = "/", data = null, options = {}) {
+    options.method = "POST";
+    options.json = data;
+    return this.send(url, options);
+  }
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  put(url = "/", data = null, options = {}) {
+    options.method = "PUT";
+    options.json = data;
+    return this.send(url, options);
+  }
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  patch(url = "/", data = null, options = {}) {
+    options.method = "PATCH";
+    options.json = data;
+    return this.send(url, options);
+  }
+
+  /**
+   * @param {string} url
+   * @param {?object} data
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  form(url = "/", data = null, options = {}) {
+    options.method = "POST";
+    options.form = data;
+    return this.send(url, options);
+  }
+}
---
+++ src/FetchQueue.js
@@ -0,0 +1,238 @@
+/**
+ *
+ * @callback handleErrors
+ * @param {HttpException} error
+ *
+ * @callback fetchToken
+ * @returns {Promise<void>}
+ * @throws {HttpException}
+ *
+ * @typedef HttpResponse
+ * @type {object}
+ * @property {number} status
+ * @property {any} body
+ *
+ * @typedef Config
+ * @type {object}
+ * @property {fetchToken} [renew]
+ * @property {fetchToken} [refresh]
+ * @property {handleErrors} [handleErrors]
+ *
+ * @typedef Options
+ * @type {object}
+ * @property {string} [baseUrl]
+ * @property {string} [method]
+ * @property {AbortSignal} [signal]
+ * @property {RequestCredentials} [credentials]
+ * @property {?Object.<string, any>} [headers]
+ * @property {?Object.<string, any>} [query]
+ * @property {?Object.<string, any>} [body]
+ * @property {?Object.<string, any>} [form]
+ * @property {?Object.<string, any>} [json]
+ *
+ */
+
+export default class FetchQueue extends Fetch {
+  static NONE = 0;
+  static ASYNC = 1;
+  static SYNC = 2;
+
+  config = {};
+
+  #queue = [];
+  pending = FetchQueue.NONE;
+
+  accessStore = {
+    token: null,
+    delay: 0,
+    exp: 0,
+  };
+  refreshStore = {
+    token: null,
+    delay: 0,
+    exp: 0,
+    forceRetry: false,
+  };
+
+  /**
+   * @param {Options} options
+   * @param {Config} config
+   */
+  constructor(options = {}, config = {}) {
+    super(options);
+    this.config = config;
+  }
+
+  // ------------------------------------------------------------------
+  // Token
+  // ------------------------------------------------------------------
+
+  renew() {
+    if (!this.config.renew) {
+      return new Promise((resolve) => resolve());
+    }
+
+    this.pending = FetchQueue.ASYNC;
+
+    return this.config
+      .renew()
+      .catch(() => {
+        this.resetAccess();
+        if (this.refreshStore.exp > Date.now()) {
+          return this.refresh();
+        }
+        this.resetRefresh();
+      })
+      .finally(() => {
+        this.pending = FetchQueue.NONE;
+        this.runQueue();
+      });
+  }
+
+  refresh() {
+    if (!this.config.refresh) {
+      return new Promise((resolve) => resolve());
+    }
+
+    this.pending = FetchQueue.SYNC;
+
+    return this.config
+      .refresh()
+      .catch(() => {
+        this.resetAccess();
+        this.resetRefresh();
+      })
+      .finally(() => {
+        this.pending = FetchQueue.NONE;
+        this.runQueue();
+      });
+  }
+
+  resetAccess() {
+    this.accessStore.token = null;
+    this.accessStore.delay = 0;
+    this.accessStore.exp = 0;
+  }
+
+  resetRefresh() {
+    this.refreshStore.token = null;
+    this.refreshStore.delay = 0;
+    this.refreshStore.exp = 0;
+    this.refreshStore.forceRetry = false;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Fetch
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * @param {string} url
+   * @param {Options} options
+   * @returns {Promise<HttpResponse | void>}
+   * @throws {HttpException}
+   */
+  async send(url, options = {}) {
+    // Access token pending -----------------------------------------------------------------------
+    if (await this.isQueue()) {
+      // Add api to pending queue -----------------------------------------------------------------
+
+      return new Promise((resolve, reject) => {
+        this.#queue.push({
+          promise: () => this.send(url, options),
+          resolve,
+          reject,
+        });
+      });
+    }
+
+    // Add authorisation params -------------------------------------------------------------------
+    if (this.accessStore.token) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${this.accessStore.token}`,
+      };
+    }
+
+    return super.send(url, options).catch(async (e) => {
+      if (e.data.status === 401) {
+        this.resetAccess();
+        if (
+          this.refreshStore.exp > Date.now() ||
+          this.refreshStore.forceRetry
+        ) {
+          return this.send(url, options);
+        }
+      }
+
+      await this.handleErrors(e);
+
+      throw e;
+    });
+  }
+
+  async handleErrors(e) {
+    if (this.config.handleErrors) {
+      await this.config.handleErrors(e);
+    }
+    throw e;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Queue
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * @returns {boolean} True if need to be queue
+   */
+  async isQueue() {
+    if (this.pending === FetchQueue.SYNC) {
+      return true;
+    }
+
+    const timestamp = Date.now();
+
+    // access valid -------------------------------------------------------------------------------
+    if (this.accessStore.exp > timestamp) {
+      // access is older than 15 min => renew -----------------------------------------------------
+      const createdAt = this.accessStore.exp - this.accessStore.delay;
+      if (
+        createdAt < timestamp - 1000 * 60 * 15 &&
+        this.pending === FetchQueue.NONE
+      ) {
+        this.renew();
+      }
+      return false;
+    }
+
+    // refresh valid ------------------------------------------------------------------------------
+    if (this.refreshStore.exp > Date.now() || this.refreshStore.forceRetry) {
+      if (this.pending === FetchQueue.NONE) {
+        this.refresh();
+      }
+      this.pending = FetchQueue.SYNC;
+      return true;
+    }
+
+    this.resetAccess();
+    this.resetRefresh();
+
+    this.runQueue();
+    return false;
+  }
+
+  runQueue() {
+    const handler = this.#queue.shift(0);
+    handler
+      ?.promise()
+      .then((response) => {
+        handler.resolve(response);
+      })
+      .catch((error) => {
+        handler.reject(error);
+      });
+
+    if (this.#queue.length) {
+      this.runQueue();
+    }
+  }
+}
